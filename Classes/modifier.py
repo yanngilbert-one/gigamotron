73d5361197a8dc9075455d609f3ab854faf3cf11
kipper86 <gobbeyrulz@hotmail.com>
2025-10-03 11:38:57 -0400

    coordinates
    New file to get coordinates
    
    changed the coordinates from hard pixels to % of screen to accommodate from 1080p or 1440p. not for ultra wide currently

--- Classes/modifier.py
+++ Classes/modifier.py
@@ -5,6 +5,3 @@ class Modifier:
     def __init__ (self, name: str, affix: str):
         self.name = name
         self.affix = affix
-
-    def __repr__(self):
-        return f'Modifier(name="{self.name}", value)'
