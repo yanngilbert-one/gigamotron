4516ba4efba6eec8f1e2636c5b249dc7dfbbe32e
kipper86 <gobbeyrulz@hotmail.com>
2025-10-03 16:45:30 -0400

    change load file
    Changing load files so that it splits prefix and suffixes in 2 lists and it reads the use=0 or use=1 to see if it should or not add it to the list

--- Resources/read_file.py
+++ Resources/read_file.py
@@ -1,22 +1,24 @@
 def read_file():
     #read the file and create an object for each line of the file. "r" to read the file and cannot write
-    modifiers_by_affix={}
-    with open("src\modifiers.txt", "r") as file:
+    prefixes = []
+    suffixes = []
+    with open("src/modifiers.txt", "r") as file:
         current_block={}
         for line in file:
             line = line.strip()
             if line.startswith("{"):
                 current_block={}
+            elif line.startswith("modifier"):
+                current_block["modifier"] = line.split("=", 1)[1].strip('"')
+            elif line.startswith("affix"):
+                current_block["affix"] = line.split("=", 1)[1].strip('"')
+            elif line.startswith("use="):
+                current_block["use"] = int(line.split("=", 1)[1])
             elif line.startswith("}"):
-                if "modifier" in current_block and "affix" in current_block:
-                    affix=current_block["affix"]
-                    name=current_block["modifier"]
-                    if affix not in modifiers_by_affix:
-                        modifiers_by_affix[affix] = []
-                    modifiers_by_affix[affix].append(name)
-            elif "=" in line:
-                key, value = line.split ("=", 1)
-                key = key.strip()
-                value = value.strip().strip('"').lower() #I am removing the "" could probably just not add them to the txt file and lower case
-                current_block[key] = value
-        return modifiers_by_affix
+                # End of block, keep only if use=1
+                if current_block.get("use", 0) == 1 and "modifier" in current_block and "affix" in current_block:
+                        if current_block["affix"].lower() == "prefix":
+                            prefixes.append(current_block["modifier"])
+                        elif current_block["affix"].lower() == "suffix":
+                            suffixes.append(current_block["modifier"])
+        return prefixes, suffixes
