cd0e8a134c6f61d839accc8b5e154bfd51acc216
yanngilbert-one <yann.gilbert@csuglobal.edu>
2025-10-03 00:05:04 -0500

    comment for kipper

--- main.py
+++ main.py
@@ -10,3 +10,61 @@ affixes = Resources.read_file.read_file()
 for affix, names in affixes.items():
     quoted_names = [f'"{name}"' for name in names]  # wrap each name in double quotes
     print(f"{affix} = [{', '.join(quoted_names)}]")
+
+
+
+
+
+
+
+
+# @kipper
+#Example of two methods, first is copying without advanced mod description, second is with advanced mod description
+'''
+Item Class: Bows
+Rarity: Magic
+Tempered Spine Bow of the Phantom
+--------
+Bow
+Physical Damage: 77-182 (augmented)
+Critical Strike Chance: 6.50%
+Attacks per Second: 1.40
+--------
+Requirements:
+Level: 64
+Dex: 212 (unmet)
+--------
+Sockets: R 
+--------
+Item Level: 87
+--------
++46 to Dexterity
+Adds 39 to 67 Physical Damage
+
+
+
+
+
+Item Class: Bows
+Rarity: Magic
+Tempered Spine Bow of the Phantom
+--------
+Bow
+Physical Damage: 77-182 (augmented)
+Critical Strike Chance: 6.50%
+Attacks per Second: 1.40
+--------
+Requirements:
+Level: 64
+Dex: 212 (unmet)
+--------
+Sockets: R 
+--------
+Item Level: 87
+--------
+{ Prefix Modifier "Tempered" (Tier: 2) — Damage, Physical, Attack }
+Adds 39(30-40) to 67(63-73) Physical Damage
+{ Suffix Modifier "of the Phantom" (Tier: 2) — Attribute }
++46(43-50) to Dexterity
+
+'''
